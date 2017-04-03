edcf793535659097e03274c61111be94e50e1d04
microshine <microshine@mail.ru>
2017-04-03 22:17:49 +0300

    Add files

---
+++ .travis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+    
+# Installing SoftHSM dependencies
+sudo apt-get update -qq
+sudo apt-get install autoconf -y
+sudo apt-get install automake -y
+sudo apt-get install libtool -y
+
+# Installing OpenSSL
+git clone https://github.com/openssl/openssl.git -b OpenSSL_1_0_2-stable
+cd openssl
+./config
+make
+sudo make install
+
+cd ..
+
+# Installing SoftHSM
+git clone https://github.com/opendnssec/SoftHSMv2.git -b develop
+cd SoftHSMv2
+sh ./autogen.sh
+./configure --with-openssl=/usr/local/ssl
+make
+sudo -E make install
+cd ..
+sudo ldconfig
+
+# initializing SoftHSM
+softhsm2-util --init-token --so-pin "12345" --pin "12345" --slot 0 --label "My slot 0"
